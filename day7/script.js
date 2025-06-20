9beb8664aaf157a2f40cb789abb339004355c450
mayur807 <mayurnagvanshi31@gmail.com>
2025-06-20 17:52:18 +0530

    updates

--- day7/script.js
+++ day7/script.js
@@ -5,9 +5,14 @@ document.querySelector("#Burger").addEventListener("click",()=>{
     if(bool){
     document.querySelector("#Burger").src = "cross.png"
     document.querySelector("#vis").style.visibility = "visible";
-    document.querySelector("nav").style.height = "13vh"
+    document.querySelector("nav").style.height = "25vh";
+    // document.querySelector(".logo").style.marginTop = ' 12px';
+    // document.querySelector(".icon").style.marginBottom =  "  36px;"
+    
     bool = false
 }else{
+    // document.querySelector(".icon").style.marginTop=  " 8.3px"
+    // document.querySelector(".logo").style.marginBottom = ' 12px'
     bool = true
     document.querySelector("nav").style.height = "6.5vh"
     document.querySelector("#vis").style.visibility = "hidden";
