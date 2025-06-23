bda53ce3e20851931402be45f80271a90f18d4ca
mayur807 <mayurnagvanshi31@gmail.com>
2025-06-23 21:13:42 +0530

    myntraClone

---
+++ day8/script.js
@@ -0,0 +1,33 @@
+// fetch('https://fakestoreapi.com/products/category/games')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+
+// fetch('https://fakestoreapi.com/carts/1')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// fetch('https://dummyjson.com/products/')
+// .then(res => res.json())
+
+
+// fetch('https://dummyjson.com/products?limit=200&skip=0&select=products')
+// .then(res => res.json())
+// .then(data => console.log(data));
+
+// fetch('https://dummyjson.com/products/categories/')
+// .then(res => res.json())
+// .then(console.log);
+
+// fetch('https://dummyjson.com/products/?limit=200&skip=0&select=category')
+// .then(res => res.json())
+// .then(console.log);
+
+// fetch('https://dummyjson.com/products?sortBy=title&order=asc')
+// .then(res => res.json())
+// .then(console.log);
+
+fetch('https://dummyjson.com/products/category-list')
+.then(res => res.json())
+.then(console.log);
